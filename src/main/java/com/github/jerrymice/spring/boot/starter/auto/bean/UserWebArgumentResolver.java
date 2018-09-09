4ca0b5b49d590180a2a9d9cfea3b21c2e6136be1
jerrymice <9404744@qq.com>
2018-09-09 15:08:03 +0800

    增加一个系统的当前用户存在多个不同的Class类型时的可配置处理

--- src/main/java/com/github/jerrymice/spring/boot/starter/auto/bean/UserWebArgumentResolver.java
+++ src/main/java/com/github/jerrymice/spring/boot/starter/auto/bean/UserWebArgumentResolver.java
@@ -5,21 +5,32 @@ import org.springframework.web.bind.support.WebArgumentResolver;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author tumingjian
- * SpringMVC参数解析拦截器
- * 返回当前用户信息
+ *         SpringMVC参数解析拦截器
+ *         返回当前用户信息
  */
 public class UserWebArgumentResolver implements WebArgumentResolver {
     /**
      * 当前用户在session中的key值
      */
     private String userSessionKey;
+    /**
+     * 是否缓存用户对象的class类型,如果存在多种不同类型的用户登录同一系统,那么建议设置为false
+     */
+    private boolean enableCacheUserClass;
 
     public UserWebArgumentResolver(String userSessionKey) {
-        this.userSessionKey = userSessionKey;
+        this(userSessionKey, false);
     }
+
+    public UserWebArgumentResolver(String userSessionKey, boolean enableCacheUserClass) {
+        this.userSessionKey = userSessionKey;
+        this.enableCacheUserClass = enableCacheUserClass;
+    }
+
     /**
      * 缓存当前用户类
      */
@@ -27,23 +38,32 @@ public class UserWebArgumentResolver implements WebArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter methodParameter, NativeWebRequest webRequest) throws Exception {
-        if (cacheCurrentUserClass != null) {
+        if (cacheCurrentUserClass != null || !enableCacheUserClass) {
             if (methodParameter.getParameterType() != null && cacheCurrentUserClass.isAssignableFrom(methodParameter.getParameterType())) {
-                HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-                Object currentUser = request.getSession().getAttribute(userSessionKey);
-                return currentUser;
+                return getCurrentUser(webRequest);
             }
         } else {
-            HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-            Object currentUser = request.getSession().getAttribute(userSessionKey);
+            Object currentUser = getCurrentUser(webRequest);
             if (currentUser != null) {
-                cacheCurrentUserClass = currentUser.getClass();
+                if (enableCacheUserClass) {
+                    cacheCurrentUserClass = currentUser.getClass();
+                }
                 if (methodParameter.getParameterType() != null && cacheCurrentUserClass.isAssignableFrom(methodParameter.getParameterType())) {
                     return currentUser;
                 }
             }
         }
         return UNRESOLVED;
+    }
+
+    /**
+     * 获取当前用户.
+     * @param webRequest
+     * @return
+     */
+    private Object getCurrentUser(NativeWebRequest webRequest) {
+        HttpSession session = webRequest.getNativeRequest(HttpServletRequest.class).getSession();
+        return session == null ? null : session.getAttribute(userSessionKey);
     }
 
 }
