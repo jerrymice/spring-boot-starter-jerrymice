8a0fc22461416ee2a674f09d0a2d53a2c697a647
jerrymice <9404744@qq.com>
2018-09-09 13:55:11 +0800

    修复注释

--- src/main/java/com/github/jerrymice/spring/boot/starter/EnableJerrymiceSpringBootConfiguration.java
+++ src/main/java/com/github/jerrymice/spring/boot/starter/EnableJerrymiceSpringBootConfiguration.java
@@ -32,33 +32,33 @@ public @interface EnableJerrymiceSpringBootConfiguration {
     /**
      * 是否启用扩展SessionStrategy,默认true
      */
-    String WEB_SESSION_STRATEGY_ENABLE = "jerrymice.spring.boot.config.web.session-strategy";
+    public final static String WEB_SESSION_STRATEGY_ENABLE = "jerrymice.spring.boot.config.web.session-strategy";
     /**
      * 是否启用跨域,默认true
      */
-    String WEB_CORS_MAPPINGS_ENABLE = "jerrymice.spring.boot.config.web.cors-mappings";
+    public final static String WEB_CORS_MAPPINGS_ENABLE = "jerrymice.spring.boot.config.web.cors-mappings";
     /**
      * 是否启用默认的静态资源路径,默认true
      */
-    String WEB_RESOURCE_HANDLER_ENABLE = "jerrymice.spring.boot.config.web.resource-handler";
+    public final static String WEB_RESOURCE_HANDLER_ENABLE = "jerrymice.spring.boot.config.web.resource-handler";
     /**
      * 是否启用默认的SqlSessionFactory,默认true
      */
-    String WEB_SQL_SESSIONF_FACTORY_ENABLE = "jerrymice.spring.boot.config.sql.session-factory";
+    public final static String WEB_SQL_SESSIONF_FACTORY_ENABLE = "jerrymice.spring.boot.config.sql.session-factory";
     /**
      * 是否启用默认的MessageConverters,默认true
      */
-    String WEB_MESSAGE_CONVERTERS_ENABLE = "jerrymice.spring.boot.config.web.message-converters";
+    public final static String WEB_MESSAGE_CONVERTERS_ENABLE = "jerrymice.spring.boot.config.web.message-converters";
     /**
      * 是否自动注入UserWebArgumentResolver,可以直接在Controller中获取当前用户
      */
-    String WEB_USER_ARGUMENT_RESOLVER_ENABLE = "jerrymice.spring.boot.config.web.user-argument-resolver";
+    public final static String WEB_USER_ARGUMENT_RESOLVER_ENABLE = "jerrymice.spring.boot.config.web.user-argument-resolver";
     /**
      * 是否启用security权限控制
      */
-    String WEB_SECURITY = "jerrymice.spring.boot.config.web.security.enabled";
+    public final static String WEB_SECURITY = "jerrymice.spring.boot.config.web.security.enabled";
     /**
      * 是否启用定时任务接口
      */
-    String WEB_TASK="jerrymice.spring.boot.config.web.task.enabled";
+    public final static String WEB_TASK="jerrymice.spring.boot.config.web.task.enabled";
 }
