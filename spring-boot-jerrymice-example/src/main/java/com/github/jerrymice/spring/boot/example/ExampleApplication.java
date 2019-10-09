0dad2c2a4daf58edd904b57195cd3245d30df925
jerrymice <9404744@qq.com>
2019-10-09 15:44:11 +0800

    添加spring-boot-starter-jerrymice

--- spring-boot-jerrymice-example/src/main/java/com/github/jerrymice/spring/boot/example/ExampleApplication.java
+++ spring-boot-jerrymice-example/src/main/java/com/github/jerrymice/spring/boot/example/ExampleApplication.java
@@ -1,5 +1,4 @@
 package com.github.jerrymice.spring.boot.example;
-import com.github.jerrymice.spring.boot.EnableJerryMice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
