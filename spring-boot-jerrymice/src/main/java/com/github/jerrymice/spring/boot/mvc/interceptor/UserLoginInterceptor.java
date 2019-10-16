49d0847e95bc85552c8550c9824680dbe408991c
jerrymice <9404744@qq.com>
2019-10-16 12:16:29 +0800

    去掉无用import

--- spring-boot-jerrymice/src/main/java/com/github/jerrymice/spring/boot/mvc/interceptor/UserLoginInterceptor.java
+++ spring-boot-jerrymice/src/main/java/com/github/jerrymice/spring/boot/mvc/interceptor/UserLoginInterceptor.java
@@ -1,13 +1,8 @@
 package com.github.jerrymice.spring.boot.mvc.interceptor;
 
-import com.github.jerrymice.spring.boot.EnableJerryMice;
 import com.github.jerrymice.spring.boot.mvc.properties.SpringWebMvcProperties;
-import com.github.jerrymice.spring.boot.mvc.result.ResultWrapHandler;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.ResolvableType;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -24,13 +19,11 @@ public class UserLoginInterceptor implements HandlerInterceptor {
     private String userSessionKey;
     private HttpMessageConverter<Object> converter;
     private InterceptUserHandler handler;
-    private Environment environment;
     public UserLoginInterceptor(ApplicationContext applicationContext) {
         this.userSessionKey = applicationContext.getBean(SpringWebMvcProperties.LoginInterceptor.class).getUserSessionKey();
         String[] beanNamesForType = applicationContext.getBeanNamesForType(ResolvableType.forClassWithGenerics(HttpMessageConverter.class, Object.class));
         this.converter = (HttpMessageConverter) applicationContext.getBean(beanNamesForType[0]);
         this.handler = applicationContext.getBean(InterceptUserHandler.class);
-        this.environment = applicationContext.getBean(Environment.class);
     }
 
     @Override
